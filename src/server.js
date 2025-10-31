561113e0a3db7588635ed4e46964229bbbe7c9de
Victor <victorgabrielcruzpereira21@gmail.com>
2025-10-31 16:27:23 -0300

    ..

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://seu-frontend.onrender.com', // Substitua pelo seu frontend no Render
+    'https://www.victorgabrielcruzpereira.com.br', // Substitua pelo seu frontend no Render
     'https://victorgabriel-portfolio.onrender.com' // Exemplo
   ],
   credentials: true
